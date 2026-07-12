00a68ef0ce5270e59735884b01a72aeb2c198ec6
Hongyu Chen <hongyuchen1030@gmail.com>
2026-07-11 17:06:46 -0700

    fix sum_of_squares_compensated

--- include/accusphgeom/numeric/eft.hpp
+++ include/accusphgeom/numeric/eft.hpp
@@ -47,6 +47,13 @@ inline Expansion2<T> two_sum(T a, T b) {
 }
 
 template <typename T>
+inline Expansion2<T> fast_two_sum(T a, T b) {
+  T x = a + b;
+  T y = (a - x) + b;
+  return {x, y};
+}
+
+template <typename T>
 inline Expansion2<T> accurate_difference_of_products(T a, T b, T c, T d) {
   const Expansion2<T> prod1 = two_prod_fma(a, b);
   const T neg_d = -d;
@@ -68,18 +75,47 @@ inline Expansion2<T> compensated_dot_product(const std::array<T, N>& lhs,
   return acc;
 }
 
+
 template <typename T, std::size_t N>
-inline Expansion2<T> sum_of_squares_c(const std::array<T, N>& high,
-                                      const std::array<T, N>& low) {
-  std::array<T, 2 * N> lhs{};
-  std::array<T, 2 * N> rhs{};
-  for (std::size_t i = 0; i < N; ++i) {
-    lhs[2 * i] = high[i];
-    rhs[2 * i] = high[i];
-    lhs[2 * i + 1] = low[i];
-    rhs[2 * i + 1] = low[i];
+inline T accurate_dot_product_fma(const std::array<T, N>& lhs,
+                                             const std::array<T, N>& rhs) {
+  Expansion2<T> acc = two_prod_fma(lhs[0], rhs[0]);
+  for (std::size_t i = 1; i < N; ++i) {
+    const Expansion2<T> prod = two_prod_fma(lhs[i], rhs[i]);
+    const Expansion2<T> sum = two_sum(acc.hi, prod.hi);
+    acc.hi = sum.hi;
+    acc.lo += prod.lo + sum.lo;
   }
-  return compensated_dot_product(lhs, rhs);
+  return acc.hi + acc.lo;
+}
+
+template <typename T>
+inline Expansion2<T> sum_non_neg(const Expansion2<T> & A,
+                                    const Expansion2<T> & B) {
+  const T Ah = A.hi;
+  const T Al = A.lo;
+  const T Bh = B.hi;
+  const T Bl = B.lo;
+  auto [H, h] = two_sum(Ah, Bh);
+  T c = Al + Bl;
+  T d = h + c;
+  return fast_two_sum(H, d);
+}
+
+template <typename T, std::size_t N>
+inline Expansion2<T> sum_of_squares_c(const std::array<T, N>& vals,
+                                      const std::array<T, N>& errs) {
+  T S = T(0), s = T(0);
+  for (std::size_t i = 0; i < N; ++i) {
+    auto [P, p] = two_prod_fma(vals[i], vals[i]);
+    const Expansion2<T> sum =
+        sum_non_neg(Expansion2<T>{S, s}, Expansion2<T>{P, p});
+    S = sum.hi;
+    s = sum.lo;
+  }
+  T R = accurate_dot_product_fma(vals, errs);
+  T err = T(2) * R + s;
+  return fast_two_sum(S, err);
 }
 
 template <typename T>
